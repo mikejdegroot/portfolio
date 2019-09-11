3f66d59f79cd2ec8182f2d98a4dbf8975331732f
Mike de Groot <mike.j.degroot@gmail.com>
2019-09-11 02:11:47 +0100

    new layout progress

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,5 +2,18 @@
 
 $(function () {
 
-  console.log('js');
+  $('.main-nav__item').on('click', function () {
+    // if (!$(this).has('ul')) return null;
+    var $thisSub = $(this).children('.sub-menu');
+    $('.sub-menu').not($thisSub).slideUp();
+    $thisSub.slideToggle();
+  });
+
+  $('.project-trigger').on('click', function () {
+    var dataId = $(this).attr('data-id');
+    var $thisProject = $('#' + dataId);
+    $('.project').not($thisProject).removeClass('.active');
+
+    $thisProject.toggleClass('active');
+  });
 });
