18ef65ecf7f0fbfcae5cb4eabe41abe85fa0fd90
Mike de Groot <mike.j.degroot@gmail.com>
2020-02-02 18:43:51 +0000

    removing js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,19 +1,3 @@
-'use strict';
+"use strict";
 
-$(function () {
-
-  $('.main-nav__item').on('click', function () {
-    // if (!$(this).has('ul')) return null;
-    var $thisSub = $(this).children('.sub-menu');
-    $('.sub-menu').not($thisSub).slideUp();
-    $thisSub.slideToggle();
-  });
-
-  $('.project-trigger').on('click', function () {
-    var dataId = $(this).attr('data-id');
-    var $thisProject = $('#' + dataId);
-    $('.project').not($thisProject).removeClass('.active');
-
-    $thisProject.toggleClass('active');
-  });
-});
+$(function () {});
--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,2 @@
 $(() => {
-
-  $('.main-nav__item').on('click', function() {
-    // if (!$(this).has('ul')) return null;
-    const $thisSub = $(this).children('.sub-menu');
-    $('.sub-menu').not($thisSub).slideUp();
-    $thisSub.slideToggle();
-  });
-
-  $('.project-trigger').on('click', function() {
-    const dataId = $(this).attr('data-id');
-    const $thisProject = $(`#${dataId}`);
-    $('.project').not($thisProject).removeClass('.active');
-
-    $thisProject.toggleClass('active');
-  });
-
-
 });
